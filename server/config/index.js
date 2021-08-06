3ee7deb8ff2418c8e70c2e2c3b8051d77f666404
dechilds1@gmail.com <dechilds1@gmail.com>
2021-08-06 18:42:19 +0200

    updated heroku link

--- server/config/index.js
+++ server/config/index.js
@@ -29,7 +29,7 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: ['http://localhost:3000', "https://globaltimes.herokuapp.com/"] // <== this will be the URL of our React app (it will be running on port 3000)
+      origin: ['http://localhost:3000', "https://hangout-react.herokuapp.com/"] // <== this will be the URL of our React app (it will be running on port 3000)
     })
   );
 
